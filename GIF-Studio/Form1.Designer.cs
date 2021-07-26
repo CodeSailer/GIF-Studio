5e511a3d199a8ea23084570a688726cad86d4c12
CodeSailer <76685737+CodeSailer@users.noreply.github.com>
2021-07-26 22:36:52 +0200

    Initial

---
+++ GIF-Studio/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace GIF_Studio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView1
+            // 
+            this.listView1.AllowDrop = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(351, 381);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(369, 309);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 84);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Export";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save GIF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 59);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(62, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "HQ GIF";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(375, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(242, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(375, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(542, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = ">>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "FPS:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 409);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "GIF Studio";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GIF-Studio/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GIF_Studio
+{
+    public partial class Form1 : Form
+    {
+        public ImageList imgs = new ImageList();
+        public int curr_img = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            listView1.DragDrop += listView1_DragDrop;
+            listView1.DragEnter += listView1_DragEnter;
+            listView1_Initialize();
+
+        }
+
+        private void listView1_Initialize()
+        {
+            imgs.ImageSize = new Size(50, 50);
+            listView1.View = View.Details;
+            listView1.Columns.Add("Files", listView1.Width - 10);
+        }
+
+        private void DragDrop_GetFiles(DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            Array.Sort(files);
+            imgs.Images.Clear();
+            curr_img = 0;
+            foreach (string img_path in files)
+            {
+                imgs.Images.Add(Image.FromFile(img_path));
+            }
+
+            listView1.SmallImageList = imgs;
+            listView1.Items.Clear();
+            //listView1.Items.Add("first image", 0);
+            for (int i = 0; i < files.Length; i++)
+            {
+                listView1.Items.Add(files[i].ToString(), i);
+            }
+            /*foreach (ListViewItem item in listView1.Items)
+            {
+                MessageBox.Show(item.ToString());
+            }*/
+            pictureBox1.Image = Image.FromFile(listView1.Items[curr_img].Text);
+        }
+
+        private void listView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listView1_DragDrop(object sender, DragEventArgs e)
+        {
+            DragDrop_GetFiles(e);
+        }
+
+        //next image preview
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (curr_img + 1 < listView1.Items.Count)
+            {
+                curr_img++;
+                pictureBox1.Image = Image.FromFile(listView1.Items[curr_img].Text);
+            }
+            else
+            {
+                curr_img = 0;
+                pictureBox1.Image = Image.FromFile(listView1.Items[curr_img].Text);
+            }
+        }
+
+        //previus image preview
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (curr_img - 1 >= 0)
+            {
+                curr_img--;
+                pictureBox1.Image = Image.FromFile(listView1.Items[curr_img].Text);
+            }
+            else
+            {
+                curr_img = listView1.Items.Count - 1;
+                pictureBox1.Image = Image.FromFile(listView1.Items[curr_img].Text);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //copy each images into temporary folder
+            DirectoryInfo di = new DirectoryInfo(".\\images\\");
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (ListViewItem element in listView1.Items)
+            {
+                File.Copy(element.Text, ".\\images\\" + element.Index.ToString() + ".jpg", true);
+            }
+            if (File.Exists("settings.txt"))
+            {
+                File.Delete("settings.txt");
+            }
+            if (textBox1.Text == null || !int.TryParse(textBox1.Text, out int value))
+            {
+                File.WriteAllText("settings.txt", "1");
+            }
+            else
+            {
+                File.WriteAllText("settings.txt", textBox1.Text);
+            }
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            if (checkBox1.Checked)
+            {
+                proc.StartInfo.FileName = "ToGIFHQ.bat";
+            }
+            else
+            {
+                proc.StartInfo.FileName = "ToGIF.bat";
+            }
+            proc.Start();
+        }
+    }
+}
